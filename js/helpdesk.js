e52ee58cbdded4abad53f1ffb22780a2fa6dc6ff
Stan de Horn <Stan de Horn>
2015-11-10 20:55:40 +0100

    Totally revamped the javascript for the helpdesk

--- js/helpdesk.js
+++ js/helpdesk.js
@@ -2,31 +2,34 @@
  * Created by Stan de Horn on 10/11/2015.
  */
 
-addEventListener("click", helpdesk);
+
 
 function helpdesk(){
 
-var naam;
-prompt("Wat is de naam van de klant?");
-document.write("<h1>Klant: </h1>" + naam)
-prompt("Wat is de datum van vandaag?");
-document.getElementById("datum").innerHTML = datum;
-prompt("Wat is de modem van de klant?");
-document.getElementById("modem").innerHTML = modem;
-var klacht = confirm("Heeft u een klacht over ons product Internet?");
-if (klacht == true) {
-    klacht2 = "Klant meldt een probleem met internet";
-} else {
-    klacht3 = "You pressed Cancel!";
-}
-document.getElementById("klacht2").innerHTML = klacht2;
-document.getElementById("klacht3").innerHTML = klacht3;
-var nunl = confirm("Kunt u browsen naar www.nu.nl");
-if (klacht == true) {
-    nunl2 = "Browsen naar nu.nl lukt";
-} else {
-    nunl3 = "Browsen naar nu.nl lukt niet";
-}
-document.getElementById("nunl2").innerHTML = nunl2;
-document.getElementById("nunl3").innerHTML = nunl3;
+    var naam = prompt("Wat is de naam van de klant?");
+        document.getElementById("nu_nl").innerHTML = null;
+    var datum = prompt("Wat is de datum van vandaag?");
+
+    var modem = prompt("Wat is de modem van de klant?");
+    /*
+    var klacht = confirm("Heeft u een klacht over ons product Internet?");
+    if (klacht == true) {
+        document.write("<p><i class='fa fa-warning'></i> Klant meldt een probleem met internet</p><br>");
+    }   else {
+        window.location = "helpdesk2.html";
+    }
+
+    var nu_nl = confirm("Kunt u browsen naar www.nu.nl");
+    if (nu_nl == true) {
+        document.writeln("<p><i class='fa fa-check'> </i>Browsen naar nu.nl lukt</p><br>");
+    }   else {
+        document.writeln("<p><i class='fa fa-warning'> </i> Browsen naar nu.nl lukt niet</p><br>");
+    }
+
+    var nu_nl_reload = confirm("Reageert de pagina snel na het indrukken van F5");
+    if (nu_nl_reload == true) {
+        document.writeln("<p><i class='fa fa-check'> </i>De pagina reageert snel als F5 wordt ingedrukt</p><br>");
+    }   else {
+        document.writeln("<p><i class='fa fa-warning'> </i> De pagina reageert niet of langzaam als F5 ingedrukt wordt</p><br>");
+    } */
 }
